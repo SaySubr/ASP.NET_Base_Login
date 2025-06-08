a93bad1bd72746b80d962b88e9fd264a9c7a9706
Saymon <165163931+SaySubr@users.noreply.github.com>
2025-06-08 15:44:21 +0700

    3 com

---
+++ User/DBUser.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeWork.User
+{
+    public class DBUser
+    {
+        public int id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
